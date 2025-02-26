3748168691c17ec252d86b8ff896387e62088605
Abdullah Karagoz <abdullahkaraqoz@outlook.com>
2025-02-26 22:33:29 +0300

    - Updated solution file to include new projects: src, services, catalog, and NewMicroservice.Catalog.Api. - Created .dockerignore and .env files for Docker configuration. - Introduced docker-compose.dcproj and docker-compose.yml for MongoDB and Mongo Express services. - Added launchSettings.json for environment profiles and appsettings files for logging configuration. - Updated NewMicroservice.Catalog.Api.csproj to target .NET 8.0 with necessary package references. - Created Program.cs for application setup and Swagger integration. - Added entity classes Category and Course, along with a BaseEntity class. - Included a sample API request in NewMicroservice.Catalog.Api.http.

---
+++ NewMicroservice.Catalog.Api/Repositories/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace NewMicroservice.Catalog.Api.Repositories;
+public class BaseEntity
+{
+    [BsonElement("_id")] public Guid Id { get; set; }
+}
