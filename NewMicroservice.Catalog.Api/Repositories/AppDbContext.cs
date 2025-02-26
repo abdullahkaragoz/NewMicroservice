9bb37c5005202d3b4cbab471889075f438eea466
Abdullah Karagoz <abdullahkaraqoz@outlook.com>
2025-02-26 22:58:29 +0300

    Refactor Course and Feature entities with configurations

---
+++ NewMicroservice.Catalog.Api/Repositories/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+using NewMicroservice.Catalog.Api.Features.Categories;
+using NewMicroservice.Catalog.Api.Features.Courses;
+using System.Reflection;
+
+namespace NewMicroservice.Catalog.Api.Repositories;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+}
---
+++ NewMicroservice.Catalog.Api/Repositories/CategoryEntityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using NewMicroservice.Catalog.Api.Features.Categories;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace NewMicroservice.Catalog.Api.Repositories;
+
+public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToCollection("categories");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever();
+        builder.Ignore(x => x.Courses);
+    }
+}
